8a91ffcfcca7ad98007b5d50b6d10b8de667ad05
wheyjanc <wchen93@asu.edu>
2018-09-11 16:01:46 -0400

    modified prettier config

--- server/api/products.js
+++ server/api/products.js
@@ -1,23 +1,23 @@
 'use strict'
 
 const express = require('express')
-const {Product, Category} = require('../db/models')
+const { Product, Category } = require('../db/models')
 
 const router = express.Router()
 
 // route to serve up all products
 router.get('/', (req, res, next) => {
   Product.findAll({
-    include: [{model: Category, as: 'category'}]
+    include: [{ model: Category, as: 'category' }]
   })
     .then(products => res.status(200).json(products))
     .catch(next)
 })
 
-// route to serve up a single product
+// route to serve up a single product by id
 router.get('/:id', (req, res, next) => {
   Product.findById(req.params.id, {
-    include: [{model: Category, as: 'category'}]
+    include: [{ model: Category, as: 'category' }]
   })
     .then(product => res.status(200).json(product))
     .catch(next)
