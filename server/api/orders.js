8862b8270806fd3d57a128b2e8e6f4910318aebe
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-09-12 15:56:44 -0400

    Modified orders routes to load products associated with orders

--- server/api/orders.js
+++ server/api/orders.js
@@ -3,20 +3,26 @@ const { Order } = require('../db/models')
 
 //retrieves all orders from database
 //TODO:  Restrict access to admin users
-router.get('/', (req, res, next) => {
-  Order.findAll()
-    .then(orders => res.status(200).json(orders))
-    .catch(next)
-})
-
 router.get('/:userId', (req, res, next) => {
   Order.findAll({
     where: {
-      userId: req.params.userId
-    }
+      userId: null
+    },
+    include: [{all: true}]
   })
     .then(orders => res.status(200).json(orders))
     .catch(next)
 })
+
+router.get('/', (req, res, next) => {
+  Order.findAll({
+    include: [{all: true}]
+  })
+    .then(orders => {
+      return res.status(200).json(orders)
+    })
+    .catch(next)
+})
+
 
 module.exports = router
