4b7ac458cbb4014244845cab21161980df14655d
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-09-12 11:15:27 -0400

    Adds route that retrieves orders by user id

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,10 +1,21 @@
 const router = require('express').Router()
 const { Order } = require('../db/models')
 
+//retrieves all orders from database
+//TODO:  Restrict access to admin users
 router.get('/', (req, res, next) => {
-  //retrieve orders from database
   Order.findAll()
-    .then(orders => res.json(orders))
+    .then(orders => res.status(200).json(orders))
     .catch(next)
 })
+
+router.get('/:userId', (req, res, next) => {
+  Order.findAll({
+    where: {
+      userId: req.params.userId
+    }
+  })
+    .then(orders => res.status(200).json(orders))
+})
+
 module.exports = router
