2dee426792ad52aba67c3f81e9673e676ff6d8bf
Stella Yun <stellasyyun@gmail.com>
2018-09-12 11:39:02 -0400

    Added component and reducer to see all users at path /users

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -13,6 +13,18 @@ const AuthForm = props => {
     <div>
       <form onSubmit={handleSubmit} name={name}>
         <div>
+          <label htmlFor="firstName">
+            <small>First Name</small>
+          </label>
+          <input name="firstName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName">
+            <small>Last Name</small>
+          </label>
+          <input name="lastName" type="text" />
+        </div>
+        <div>
           <label htmlFor="email">
             <small>Email</small>
           </label>
@@ -23,6 +35,18 @@ const AuthForm = props => {
             <small>Password</small>
           </label>
           <input name="password" type="password" />
+        </div>
+        <div>
+          <label htmlFor="billingAddress">
+            <small>Billing address</small>
+          </label>
+          <input name="billingAddress" type="text" />
+        </div>
+        <div>
+          <label htmlFor="shippingAddress">
+            <small>Shipping address</small>
+          </label>
+          <input name="shippingAddress" type="text" />
         </div>
         <div>
           <button type="submit">{displayName}</button>
