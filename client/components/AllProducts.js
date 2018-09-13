726a0eda1bef1412398f7f5f11f81b7fd7738caa
wheyjanc <wchen93@asu.edu>
2018-09-13 10:47:59 -0400

    commit to merge

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -4,13 +4,42 @@ import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
 import GridListTileBar from '@material-ui/core/GridListTileBar'
 import ListSubheader from '@material-ui/core/ListSubheader'
+import TextField from '@material-ui/core/TextField'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
 
-export default class AllProducts extends Component {
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  }
+})
+
+class AllProducts extends Component {
+  constructor() {
+    super()
+    this.state = {
+      search: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
   componentDidMount() {
     this.props.loadAllProducts()
   }
 
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
+    const { classes } = this.props
     const products = this.props.products
     return (
       <div className="container">
@@ -36,3 +65,9 @@ export default class AllProducts extends Component {
     )
   }
 }
+
+TextField.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(AllProducts)
--- server/api/products.js
+++ server/api/products.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const express = require('express')
-
 const { Product, Category } = require('../db/models')
-
 const router = express.Router()
+const url = require('url')
+const querystring = require('querystring')
+const bodyParser = require('body-parser')
 
 // route to serve up all products
 router.get('/', (req, res, next) => {
@@ -14,7 +15,6 @@ router.get('/', (req, res, next) => {
     .then(products => res.status(200).json(products))
     .catch(next)
 })
-
 
 // route to serve up a single product by id
 router.get('/:id', (req, res, next) => {
