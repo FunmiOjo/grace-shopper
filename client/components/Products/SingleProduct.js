6e6aed4986d86a9a1182c7826670d8297d459a26
Stella Yun <stellasyyun@gmail.com>
2018-09-18 16:23:07 -0400

    Fixed

--- client/components/Products/SingleProduct.js
+++ client/components/Products/SingleProduct.js
@@ -33,7 +33,6 @@ class SingleProduct extends Component {
   }
 
   componentDidMount() {
-    window.scrollTo(0, 0)
     this.props.fetchReview(this.props.match.params.productId)
     this.props.loadSingleProduct()
   }
